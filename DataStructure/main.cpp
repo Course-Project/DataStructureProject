6bb899a7b103ad281ff9f2f87e9eec82707cebca
h1994st <h1994st@gmail.com>
2014-07-14 09:41:38 +0800

    Initial Commit

---
+++ DataStructure/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  DataStructure
+//
+//  Created by Tom Hu on 7/14/14.
+//  Copyright (c) 2014 Tom Hu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
